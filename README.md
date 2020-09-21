54fb8b5be83fd5112dabc65773c017728a971397
Suraj Patil <surajp815@gmail.com>
2020-09-21 11:00:39 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# DistilBart-MNLI
+
+This distillbart-mnli is the distilled version of bart-large-mnli created using the **No Teacher Distillation** technique proposed for BART summarisation by Huggingface, [here](https://github.com/huggingface/transformers/tree/master/examples/seq2seq#distilbart).
+
+We just copy alternating layers from `bart-large-mnli` and finetune more on the same data. 
+
+
+|                                                                                      | matched acc | mismatched acc |
+| ------------------------------------------------------------------------------------ | ----------- | -------------- |
+| [bart-large-mnli](https://huggingface.co/facebook/bart-large-mnli) (baseline, 12-12) | 89.9        | 90.01          |
+| [distilbart-mnli-12-1](https://huggingface.co/valhalla/distilbart-mnli-12-1)         | 87.08       | 87.5           |
+| [distilbart-mnli-12-3](https://huggingface.co/valhalla/distilbart-mnli-12-3)         | 88.1        | 88.19          |
+| [distilbart-mnli-12-6](https://huggingface.co/valhalla/distilbart-mnli-12-6)         | 89.19       | 89.01          |
+| [distilbart-mnli-12-9](https://huggingface.co/valhalla/distilbart-mnli-12-9)         | 89.56       | 89.52          |
+
+
+This is a very simple and effective technique, as we can see the performance drop is very little.
+
+
+## Fine-tuning
+
+Clone and install transformers from source
+```bash
+git clone https://github.com/huggingface/transformers.git
+pip install -qqq -U ./transformers
+```
+
+Download MNLI data
+```bash
+python transformers/utils/download_glue_data.py --data_dir glue_data --tasks MNLI
+```
+
+Create student model
+```bash
+python create_student.py \
+  --teacher_model_name_or_path facebook/bart-large-mnli \
+  --student_encoder_layers 12 \
+  --student_decoder_layers 6 \
+  --save_path student-bart-mnli-12-6 \
+```
+
+Start fine-tuning
+```bash
+python run_glue.py args.json
+```
+
+You can find the logs of these trained models in this [wandb project](https://wandb.ai/psuraj/distilbart-mnli).
---
+++ trainer.py
@@ -0,0 +1,103 @@
+import warnings
+from typing import Any, Callable, Dict, List, Optional, Tuple, Union
+
+import numpy as np
+import torch
+from torch.utils.data.dataloader import DataLoader
+from tqdm.auto import tqdm
+
+from transformers import Trainer as HFTrainer
+from transformers.utils import logging
+from transformers.trainer_utils import (
+    EvalPrediction,
+    PredictionOutput,
+    nested_concat,
+    nested_numpify,
+)
+
+logger = logging.get_logger(__name__)
+
+class Trainer(HFTrainer):
+    def prediction_loop(
+        self, dataloader: DataLoader, description: str, prediction_loss_only: Optional[bool] = None
+    ) -> PredictionOutput:
+        """
+        Prediction/evaluation loop, shared by :obj:`Trainer.evaluate()` and :obj:`Trainer.predict()`.
+
+        Works both with or without labels.
+        """
+        if hasattr(self, "_prediction_loop"):
+            warnings.warn(
+                "The `_prediction_loop` method is deprecated and won't be called in a future version, define `prediction_loop` in your subclass.",
+                FutureWarning,
+            )
+            return self._prediction_loop(dataloader, description, prediction_loss_only=prediction_loss_only)
+
+        prediction_loss_only = (
+            prediction_loss_only if prediction_loss_only is not None else self.args.prediction_loss_only
+        )
+
+        assert not getattr(
+            self.model.config, "output_attentions", False
+        ), "The prediction loop does not work with `output_attentions=True`."
+        assert not getattr(
+            self.model.config, "output_hidden_states", False
+        ), "The prediction loop does not work with `output_hidden_states=True`."
+
+        model = self.model
+        # multi-gpu eval
+        if self.args.n_gpu > 1:
+            model = torch.nn.DataParallel(model)
+        else:
+            model = self.model
+        # Note: in torch.distributed mode, there's no point in wrapping the model
+        # inside a DistributedDataParallel as we'll be under `no_grad` anyways.
+
+        batch_size = dataloader.batch_size
+        logger.info("***** Running %s *****", description)
+        logger.info("  Num examples = %d", self.num_examples(dataloader))
+        logger.info("  Batch size = %d", batch_size)
+        eval_losses: List[float] = []
+        preds: torch.Tensor = None
+        label_ids: torch.Tensor = None
+        model.eval()
+
+        if self.args.past_index >= 0:
+            self._past = None
+
+        disable_tqdm = not self.is_local_process_zero() or self.args.disable_tqdm
+        for inputs in tqdm(dataloader, desc=description, disable=disable_tqdm):
+            loss, logits, labels = self.prediction_step(model, inputs, prediction_loss_only)
+            batch_size = inputs[list(inputs.keys())[0]].shape[0]
+            if loss is not None:
+                eval_losses.extend([loss] * batch_size)
+            if logits is not None:
+                logits = logits[0]
+                preds = logits if preds is None else nested_concat(preds, logits, dim=0)
+            if labels is not None:
+                label_ids = labels if label_ids is None else nested_concat(label_ids, labels, dim=0)
+
+        if self.args.past_index and hasattr(self, "_past"):
+            # Clean the state at the end of the evaluation loop
+            delattr(self, "_past")
+
+
+        # Finally, turn the aggregated tensors into numpy arrays.
+        if preds is not None:
+            preds = nested_numpify(preds)
+        if label_ids is not None:
+            label_ids = nested_numpify(label_ids)
+
+        if self.compute_metrics is not None and preds is not None and label_ids is not None:
+            metrics = self.compute_metrics(EvalPrediction(predictions=preds, label_ids=label_ids))
+        else:
+            metrics = {}
+        if len(eval_losses) > 0:
+            metrics["eval_loss"] = np.mean(eval_losses)
+
+        # Prefix all keys with eval_
+        for key in list(metrics.keys()):
+            if not key.startswith("eval_"):
+                metrics[f"eval_{key}"] = metrics.pop(key)
+
+        return PredictionOutput(predictions=preds, label_ids=label_ids, metrics=metrics)
